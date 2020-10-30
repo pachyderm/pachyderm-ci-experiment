a80a7bf7267e3a761c10b0a68a83d6910be213b6
Luke Marsden <me@lukemarsden.net>
2020-10-30 13:58:26 +0000

    try ssh'ing directly to vm instead of going via websockets

--- etc/testing/github_tests.sh
+++ etc/testing/github_tests.sh
@@ -52,7 +52,7 @@ echo "Running test suite based on BUCKET=$BUCKET"
 #make docker-build
 for X in worker pachd; do
     echo "Copying pachyderm/$X:local to kube"
-    docker save pachyderm/$X:local |gzip | pv | testctl ssh --tty=false -- sh -c 'gzip -d | docker load'
+    docker save pachyderm/$X:local |gzip | pv | ../testing/testctl-ssh.sh -- sh -c 'gzip -d | docker load'
 done
 make launch-dev
 
