881da896bbd000e20e4c33eed8de4b210ba6cc7b
Luke Marsden <me@lukemarsden.net>
2020-11-02 15:15:11 +0000

    debug

--- etc/testing/testctl-ssh.sh
+++ etc/testing/testctl-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -xeuo pipefail
 IP=$(grep "##EXTERNAL_IP=" "$KUBECONFIG" |cut -d '=' -f 2-)
 PORT=$(grep "##SSH_FORWARDED_PORT=" "$KUBECONFIG" |cut -d '=' -f 2-)
 grep "##PRIVATE_KEY_ONELINER=" "$KUBECONFIG" |cut -d '=' -f 2- |base64 -d > id_rsa
