474c5bd0d12e7afe65349310ce3dd2203fc7b7b7
Luke Marsden <me@lukemarsden.net>
2020-11-05 13:03:23 +0000

    Improve logging

--- etc/testing/github_tests.sh
+++ etc/testing/github_tests.sh
@@ -9,7 +9,9 @@ rm -f "${HOME}"/.pachyderm/config.json
 # Specify the slot so that future builds on this branch+suite id automatically
 # clean up previous VMs
 BRANCH="${CIRCLE_BRANCH:-$GITHUB_REF}"
-testctl get --config .testfaster.yml --slot "${BRANCH},${BUCKET}"
+echo "Getting VM."
+time testctl get --config .testfaster.yml --slot "${BRANCH},${BUCKET}"
+echo "Finished getting VM."
 
 KUBECONFIG="$(pwd)/kubeconfig"
 export KUBECONFIG
@@ -18,18 +20,21 @@ export KUBECONFIG
 # we assume 'make docker-build' has been done by a previous build step. see .circleci/config.yml
 
 # [ ]: send files across TODO: make this use rsync --delete
-./etc/testing/testctl-rsync.sh . /root/project
+echo "Copying context to runner."
+time ./etc/testing/testctl-rsync.sh . /root/project
+echo "Finished copying context."
 
 # [x]: pass environment variables through, at least ENT_ACT_CODE, BUCKET
 # [x]: pass arguments over
 
 # workaround https://serverfault.com/questions/482907/setting-a-variable-for-a-given-ssh-host
 
-./etc/testing/testctl-ssh.sh \
+echo "Starting test $BUCKET."
+time ./etc/testing/testctl-ssh.sh \
     -o SendEnv=PPS_BUCKETS \
     -o SendEnv=AUTH_BUCKETS \
     -o SendEnv=GOPROXY \
     -o SendEnv=ENT_ACT_CODE \
     -o SendEnv=BUCKET \
     -- ./project/etc/testing/github_tests_inner.sh "$@"
-
+echo "Finished test $BUCKET."
